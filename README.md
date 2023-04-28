d4632cbbb3929e0cbfba5ef14d1a638c226bf172
Marc Gilbert <46445483+MarcGilbertSparte@users.noreply.github.com>
2023-04-28 12:20:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# SparteChallenge
+
+Bienvenue sur le Sparte Challenge !
+
+## Prérequis :
+[Node 18](https://nodejs.org/fr/download/)
+
+Angular 15 : `npm i @angular/cli -g`
+
+Avant de le télécharger, Merci de créer un nouveau repo privé en utilisant ce projet comme template.
+Une fois le nouveau repo cloné sur votre espace de travail, il faudra créer une nouvelle branche avant de commencer à coder.
+
+Une fois que vous aurez fini, il vous faudra créer une pull request et m'ajouter en tant que reviewer pour que je puisse évaluer votre code.
+
+## Installation des dépendances
+`npm i`
+
+## Mise en route du projet
+`npm run start`
+
+## Instructions challenge
+Les énoncés des exercices sont sur http://localhost:4200/home
+Les endroits où du code est nécessaire sont signalés par des commentaires avec "TODO:"
+
+Il n'y a qu'à ces 7 endroits que du code est nécessaire pour résoudre les exercices,
+Je ne suis pas fermé à ce que vous sortiez des clous pour me proposer des améliorations mais ce n'est pas attendu.
+
+Il y a cependant certains fichiers qui ne doivent pas être modifiés :
+Des commentaires avec "ATTENTION:" sont situés sur ces fichiers.
+
+Il n'y a normalement pas besoin de modifier ni les fichiers html, ni les fichiers scss.
+
+Merci et bon courage !
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,57 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+interface ChallengeData {
+  name: string,
+  challenges: string[]
+}
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomePageComponent {
+  //ATTENTION: Ne pas modifier ce component ou ses méthodes
+  public challengeData: ChallengeData[];
+
+  constructor() {
+    this.challengeData = [
+      {
+        name: 'Routing',
+        challenges: [
+          '<b>TODO 1 :</b> Configurer la route <a href="./challenge">/challenge</a> dans app-routing.module.ts pour accéder à l\'épreuve.'
+        ]
+      },
+      {
+        name: 'Graphiques',
+        challenges: [
+          '<b>TODO 2 :</b> Un <b><a href="https://echarts.apache.org/examples/en/editor.html?c=pie-doughnut" target="_blank">Pie Chart</a></b> représentant le nombre de systèmes par Environnement (parents et enfants confondus).',
+          '<b>TODO 3 :</b> Un <b><a href="https://echarts.apache.org/examples/en/editor.html?c=pie-doughnut" target="_blank">Pie Chart</a></b> représentant le nombre d\'assets pour les Systèmes entre les ids [sys005...sys013] (this.systemsIdsForAssetPieChart)',
+          '<b>TODO 4 :</b> Un <b><a href="https://echarts.apache.org/examples/en/editor.html?c=line-stack" target="_blank">Stacked Line Chart</a></b> \
+           représentant l\'évolution des températures <b>par heure</b> (seulement les 24 premières) et par asset pour chacun des systèmes suivant : <br/>\
+          <ol><li>sys002: Eau</li> <li>sys005: Ligne Production</li> <li>sys006: Haute Tension</li> <li>sys007: Basse Tension</li></ol>\
+          Avec en abcisse les heures et en ordonnée les températures.<br/><b>TODO 5 :</b> Attention, la fonction <b>get recursiveAssets()</b> du modèle System n\'est pas récursive.',
+          '<b>TODO 6 et 7 :</b> Un <b><a href="https://echarts.apache.org/examples/en/editor.html?c=bar-y-category-stack" target="_blank">Stacked <u>Vertical</u> Bar Chart</a></b> représentant le nombre d\'éléments produits (data.name === "output") par Machine <b>par jour</b><br/>\
+          <ul><li>Avec en abcisse les jours</li> <li>En ordonnée la somme des éléments <b>produits par Machine</b> chaque jour</li></ul>\
+          <b>Attention :</b> dans l\'exemple en lien les axes sont inversés par rapport à l\'exercice demandé.'
+        ]
+      },
+      {
+        name: 'Conseils',
+        challenges: [
+          'Prendre le temps de bien comprendre la structure du projet.',
+          'Recherchez les commentaires comportant des <b>TODO:</b> (au nombre de 7) pour coder vos réponses. Il n\'y a pas besoin de coder ailleurs.',
+          'Il est possible de compléter ce challenge avec une quinzaine de lignes de code, sans impacter la lisibilité du projet.'
+        ]
+      },
+      {
+        name: 'Ressources',
+        challenges: [
+          '<a href="./dataset" target="_blank">Dataset Preview</a>',
+          '<a href="https://echarts.apache.org/en/option.html" target="_blank">Configuration Echart</a>'
+        ]
+      }
+    ]
+  }
+}
